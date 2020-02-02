be9123606f855a47efa704bc3741daab1c41092e
ec363 <51139488+ec363@users.noreply.github.com>
2020-02-02 20:26:34 +0000

    updated example script

--- example_script.R
+++ example_script.R
@@ -1,19 +1,33 @@
 # Example script ####
 
-# Codon table ####
+library(synonymise)
+
+# Get codon table ####
 codon_table <- loadcodontable()
 
-# Rare codon tables ####
+# Get rare codon tables (E coli B) ####
 very_rare_codons <- loadveryrarecodons()
 rare_codons <- loadrarecodons()
 
 # Take CDS as string, select alternative codons for each, and paste as string: ####
+# Note that results may vary due to sampling
 
-new1 <- synonymise(cds = "gggaaaccctag", codon_table = codon_table, forbidden_codons = c(very_rare_codons, rare_codons), frequency = 1, displacement = 0) # every codon
+# Replace every codon:
+new1 <- synonymise(cds = "GGGAAACCCTAG", codon_table = codon_table, forbidden_codons = c(very_rare_codons, rare_codons), frequency = 1, displacement = 0)
 new1
-new2 <- synonymise(cds = "gggaaaccctag", codon_table = codon_table, forbidden_codons = c(very_rare_codons, rare_codons), frequency = 2, displacement = 0) # every 2nd codon
+# example output: ggcaaaccgtag
+
+# Replace every 2nd codon:
+new2 <- synonymise(cds = "GGGAAACCCTAG", codon_table = codon_table, forbidden_codons = c(very_rare_codons, rare_codons), frequency = 2, displacement = 0)
 new2
-new3 <- synonymise(cds = "gggaaaccctag", codon_table = codon_table, forbidden_codons = c(very_rare_codons, rare_codons), frequency = 3, displacement = 0) # starts at 1
+# example output: GGGaaaCCCtag
+
+# Replace every 3rd codon, starting at codon3:
+new3 <- synonymise(cds = "GGGAAACCCTAG", codon_table = codon_table, forbidden_codons = c(very_rare_codons, rare_codons), frequency = 3, displacement = 0)
 new3
-new4 <- synonymise(cds = "gggaaaccctag", codon_table = codon_table, forbidden_codons = c(very_rare_codons, rare_codons), frequency = 3, displacement = 1) # starts at 2
+# example output: GGGAAAccaTAG
+
+# Replace every 3rd codon, starting at codon1:
+new4 <- synonymise(cds = "GGGAAACCCTAG", codon_table = codon_table, forbidden_codons = c(very_rare_codons, rare_codons), frequency = 3, displacement = 1)
 new4
+# example output: ggtAAACCCtag
